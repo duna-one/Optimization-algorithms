a8d7a0d5a6cc1bcb2884af97e3beb1d6644abd78
Даниил <korotkovdan@outlook.com>
2021-06-28 18:54:17 +0300

    Added calculation of function values

---
+++ Swarm/Functions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Swarm
+{
+    public static class Functions
+    {
+        /// <summary>
+        /// Calculates the value of the sphere function
+        /// </summary>
+        /// <param name="listX">X values for calculation</param>
+        /// <returns>F(x)</returns>
+        public static double GetSphereValue(List<double> listX)
+        {
+            double result = 0;
+            foreach (double x in listX)
+            {
+                result += x * x;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Calculates the value of the Rastrigin function
+        /// </summary>
+        /// <param name="listX">X values for calculation</param>
+        /// <returns>F(x)</returns>
+        public static double GetRastriginValue(List<double> listX)
+        {
+            double result = 0;
+            foreach (double x in listX)
+            {
+                result += (x * x) - (10 * Math.Cos(2 * Math.PI * x));
+            }
+            result *= 10 * listX.Count;
+            return result;
+        }
+
+        /// <summary>
+        /// Calculates the value of the Schwefel function
+        /// </summary>
+        /// <param name="listX">X values for calculation</param>
+        /// <returns>F(x)</returns>
+        public static double GetSchwefelValue(List<double> listX)
+        {
+            double result = 0;
+            foreach (double x in listX)
+            {
+                result += -1 * x * Math.Sin(Math.Sqrt(Math.Abs(x)));
+            }
+            return result;
+        }
+    }
+}
