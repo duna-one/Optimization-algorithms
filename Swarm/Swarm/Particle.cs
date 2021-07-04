95659983096d5acdf683490b5d031e5d51c107fb
Даниил <korotkovdan@gmail.com>
2021-07-04 21:26:20 +0300

    Fixed the namespace

--- Swarm/Swarm/Particle.cs
+++ Swarm/Swarm/Particle.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace Swarm.Swarm
+namespace Swarm
 {
     internal class Particle
     {
--- Swarm/Swarm/Swarm.cs
+++ Swarm/Swarm/Swarm.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections.Generic;
 
-namespace Swarm.Swarm
+namespace Swarm
 {
     internal class Swarm
     {
