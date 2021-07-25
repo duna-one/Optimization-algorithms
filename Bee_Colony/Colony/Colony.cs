c024e9068e5a70cd898647db223e76a6eb88ef29
Даниил <korotkovdan@gmail.com>
2021-07-25 18:33:54 +0300

    Created colony

---
+++ Bee_Colony/Colony/Colony.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+
+namespace Bee_Colony.Colony
+{
+    internal class Colony
+    {
+        #region public fields
+        public List<double> Position { get; private set; } = new List<double>();
+        public List<double> GlobalBestPosition { get; private set; } = new List<double>();
+        public double GlobalBestValue { get; private set; } = double.MaxValue;
+        #endregion
+
+        #region private fields
+        private readonly List<Scout> Scouts = new List<Scout>();
+        private readonly int SearchRadius;
+        private readonly int CheckPoints;
+        private readonly int FunctionID; 
+        private readonly int ScoutsCount;
+        #endregion
+
+        public Colony(List<double> position, int searchRadius, int scoutsCount, int checkPoints, int functionID)
+        {
+            Position = position;
+            SearchRadius = searchRadius;
+            CheckPoints = checkPoints;
+            FunctionID = functionID;
+            ScoutsCount = scoutsCount;
+
+            for (int i = 0; i < ScoutsCount; i++)
+            {
+                Scouts.Add(new Scout(GetRandomPosition(), SearchRadius, CheckPoints, FunctionID));
+            }
+        }
+
+        public void Exploration()
+        {
+            foreach (Scout scout in Scouts)
+            {
+                scout.Exploration();
+                if (scout.BestValue < GlobalBestValue)
+                {
+                    GlobalBestValue = scout.BestValue;
+                    GlobalBestPosition = scout.BestValuePosition;
+                }
+            }
+
+            Position = GlobalBestPosition;
+            Scouts.Clear();
+            for (int i = 0; i < ScoutsCount; i++)
+            {
+                Scouts.Add(new Scout(GetRandomPosition(), SearchRadius, CheckPoints, FunctionID));
+            }
+        }
+
+        private List<double> GetRandomPosition()
+        {
+            List<double> result = new List<double>();
+            for (int i = 0; i < Position.Count; i++)
+            {
+                result.Add(new System.Random().Next((int)(Position[i] - SearchRadius),
+                                                    (int)(Position[i] + SearchRadius)) +
+                           new System.Random().NextDouble());
+            }
+            return result;
+        }
+    }
+}
---
+++ Bee_Colony/Colony/Scout.cs
@@ -0,0 +1,97 @@
+﻿using Styles_Functions;
+using System.Collections.Generic;
+
+namespace Bee_Colony.Colony
+{
+    internal class Scout
+    {
+        #region public Fields
+        public List<double> Position { get; private set; } = new List<double>();
+        public double BestValue { get; private set; } = double.MaxValue;
+        public List<double> BestValuePosition { get; private set; } = new List<double>();
+        #endregion
+
+        #region private Fields
+        private readonly double SearchRadius;
+        private readonly int CheckPoints;
+        private readonly int FunctionID;
+        #endregion
+
+        #region public functions
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="position">Scout position</param>
+        /// <param name="searchRadius">Search radius</param>
+        /// <param name="checkPoints">The number of random points to check</param>
+        /// <param name="functionID">Function ID</param>
+        public Scout(List<double> position, double searchRadius, int checkPoints, int functionID)
+        {
+            Position = position;
+            SearchRadius = searchRadius;
+            CheckPoints = checkPoints;
+            FunctionID = functionID;
+        }
+
+        /// <summary>
+        /// Searches for the best value of the function at one of the random points.
+        /// Random points are specified in the parameter <see cref="CheckPoints"/>
+        /// </summary>
+        public void Exploration()
+        {
+            List<double> position;
+            double currValue;
+            for (int i = 0; i < CheckPoints; i++)
+            {
+                position = GetRandomPosition();
+                currValue = GetValue(position);
+                if (currValue < BestValue)
+                {
+                    BestValuePosition = position;
+                    BestValue = currValue;
+                }
+            }
+        }
+        #endregion
+
+        #region private functions
+
+        /// <summary>
+        /// Calculates a random location, within the radius that is specified in <see cref="SearchRadius"/>
+        /// </summary>
+        /// <returns>Coordinates of the calculated location</returns>
+        private List<double> GetRandomPosition()
+        {
+            List<double> result = new List<double>();
+            for (int i = 0; i < Position.Count; i++)
+            {
+                result.Add(new System.Random().Next((int)(Position[i] - SearchRadius),
+                                                    (int)(Position[i] + SearchRadius)) +
+                           new System.Random().NextDouble());
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Calculates the value of the function in the specified coordinates
+        /// </summary>
+        /// <param name="position">Coordinates in which to calculate the value of the function</param>
+        /// <returns>Value of the function</returns>
+        private double GetValue(List<double> position)
+        {
+            switch (FunctionID)
+            {
+                case 0:
+                    return Functions.GetSphereValue(position);
+                case 1:
+                    return Functions.GetRastriginValue(position);
+                case 2:
+                    return Functions.GetSchwefelValue(position);
+                default:
+                    throw new System.Exception("The resulting ID of the function does not exist. ID: " + FunctionID);
+            }
+        }
+        #endregion
+    }
+}
